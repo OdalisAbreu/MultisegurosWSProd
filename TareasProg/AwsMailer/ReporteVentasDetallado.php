2303e71a097fd49814a3d3940ed4167c74e0eca5
Dariel Abreu <odalisdabrue@gmail.com>
2022-03-16 22:43:59 -0400

    Reportes 3

--- TareasProg/AwsMailer/ReporteVentasDetallado.php
+++ TareasProg/AwsMailer/ReporteVentasDetallado.php
@@ -72,7 +72,7 @@ function enviarEmailHtml()
 	$mail->SMTPAuth = true;
 	$mail->Username = 'tes@aldeamo.com';
 	$mail->Password = 'vroxrVI7YS';
-	$mail->SMTPSecure = 'TLS';
+	$mail->SMTPSecure = 'TLS/STARTTLS';
 	$mail->From = 'operaciones@segurosexpress.com';
 	$mail->FromName = 'MultiSeguros';
 	$mail->Port = '2525';
