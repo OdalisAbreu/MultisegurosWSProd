ce076e64482a261b7a24a0c5e05f4a8816ff7007
Dariel Abreu <odalisdabrue@gmail.com>
2022-03-16 21:59:22 -0400

    Email Reporte Aseguradora 1.0

--- TareasProg/AwsMailer/ReporteAseguradora.php
+++ TareasProg/AwsMailer/ReporteAseguradora.php
@@ -25,7 +25,7 @@ function enviarEmailHtml($dist_id)
 	$fechasd = $ed[2] . "-" . $ed[1] . "-" . $ed[0];
 	$fecha1 = $ed[0] . "-" . $ed[1] . "-" . $ed[2];
 
-	$mail = new PHPMailer;
+/*	$mail = new PHPMailer;
 	$mail->isSMTP();
 	$mail->Host = 'multiseguros.com.do';
 	$mail->SMTPAuth = true;
@@ -35,6 +35,18 @@ function enviarEmailHtml($dist_id)
 	$mail->From = 'operaciones@multiseguros.com.do';
 	$mail->FromName = 'MultiSeguros';
 	$mail->Port = '465';
+	$mail->SMTPDebug = true;
+*/
+	$mail = new PHPMailer;
+	$mail->isSMTP();
+	$mail->Host = 'smtp.ckpnd.com';
+	$mail->SMTPAuth = true;
+	$mail->Username = 'tes@aldeamo.com';
+	$mail->Password = 'vroxrVI7YS';
+	$mail->SMTPSecure = 'ssl';
+	$mail->From = 'operaciones@segurosexpress.com';
+	$mail->FromName = 'MultiSeguros';
+	$mail->Port = '2526';
 	$mail->SMTPDebug = true;
 
 	$query = mysql_query("SELECT * FROM suplidores WHERE id_seguro ='" . $dist_id . "' LIMIT 1");
@@ -112,6 +124,104 @@ while ($p = mysql_fetch_array($sq)) {
 	$paw = mysql_fetch_array($sqaw);
 
 	if ($paw['id']) {
-		enviarEmailHtml($p['id']);
+		enviarEmailSendiu($p['id']);
 	}
+}
+
+function enviarEmailSendiu($dist_id){
+		//explode
+		$fech = fecha_despues('' . date('d/m/Y') . '', -1);
+		$ed = explode('/', $fech);
+		$fechasd = $ed[2] . "-" . $ed[1] . "-" . $ed[0];
+		$fecha1 = $ed[0] . "-" . $ed[1] . "-" . $ed[2];
+
+		$query = mysql_query("SELECT * FROM suplidores WHERE id_seguro ='" . $dist_id . "' LIMIT 1");
+		$row = mysql_fetch_array($query);
+
+		$desg = explode(",", $row['email_finanzas']);
+		$cant = count($desg);
+		$cant = $cant - 1;
+		if ($_GET['DEBUG']) {
+			echo "DEBUG";
+		} else {
+			for ($i = 0; $i <= $cant; $i++) {
+				$email = "" . $desg[$i] . "";
+			}
+		}
+
+		$email = 'odalisdabreu@gmail.com';
+		$emailCC = 'grullon.jose@gmail.com';
+
+		$archivo1 = '/ws6_3_8/TareasProg/Excel/ASEGURADORA/' . $dist_id . '/MS_RDV_' . $fechasd . '.xls';
+		$archivo2 = "/excelFiles/$dist_id/MS_EM_$fechasd.xlsx";
+		echo "$archivo1,$archivo2";
+		$subject = 'Ventas de ' . NomAseg($dist_id) . ' del ' . $fecha1 . ' ';
+
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => 'https://api.ckpnd.com:5001/v1/email',
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'POST',
+		CURLOPT_POSTFIELDS =>'{
+									"to": [
+										{
+											"email": "'.$email.'"
+										}
+									],
+									"options": {
+										"cc": [
+											{
+												"email": "'.$emailCC.'"
+											}
+										]
+									},
+									"from": {
+										"email": "operaciones@segurosexpress.com",
+										"name": "Multiseguros"
+									},
+									"replyTo": {
+										"email": "operaciones@segurosexpress.com",
+										"name": "Multiseguros"
+									},
+									"subject": "'.$subject.'",
+									"body": "
+											Buenos d&iacute;as, el archivo de las ventas esta anexado.
+											<p>
+											--------------------------------------------------------------------------------
+											<br /><br />
+											Este mensaje puede contener informaci&oacute;n privilegiada y confidencial. Dicha informaci&oacute;n es exclusivamente para el uso del individuo o entidad al cual es enviada. Si el lector de este mensaje no es el destinatario del mismo, queda formalmente notificado que cualquier divulgaci&oacute;n, distribuci&oacute;n, reproducci&oacute;n o copiado de esta comunicaci&oacute;n est&aacute; estrictamente prohibido. Si este es el caso, favor de eliminar el mensaje de su computadora e informar al emisor a trav&eacute;s de un mensaje de respuesta. Las opiniones expresadas en este mensaje son propias del autor y no necesariamente coinciden con las de MultiSeguros.<br />
+											<br />
+											<br />
+											Gracias.<br />
+											<br />
+											<br />
+											MultiSeguros
+									",
+									"attachments": [
+										{
+											"path": "'.realpath(__DIR__ . '/../../../') . $archivo1.'"
+										},
+										{
+											"path": "'.realpath(__DIR__ . '/../../../') . $archivo2.'"
+										}
+									]
+								}',
+		CURLOPT_HTTPHEADER => array(
+			'Authorization: Bearer 3f6cad2f.0f9f49318468647529d45efa',
+			'Content-Type: application/json'
+		),
+		));
+
+		$response = curl_exec($curl);
+
+		curl_close($curl);
+		echo $response;
+
+
 }
