2600da6e3ff5bd70bc92eda3984d822c9adb1148
Dariel Abreu <odalisdabrue@gmail.com>
2022-03-17 23:23:54 -0400

    EmailSendu V 1.16

--- TareasProg/AwsMailer/ReporteVentasDetallado.php
+++ TareasProg/AwsMailer/ReporteVentasDetallado.php
@@ -258,7 +258,7 @@ function enviarEmailSendiu(){
 	// BUSCAMOS VENTAS DE TODOS:
 	$UserData = TotalVentasV2($c = array('fech1' => $fdesde, 'fech2' => $fhasta));
 
-	$html =	'<table><tr><td><b>Ventas de Seguros</b></td></tr></table>';
+	$html =	'<table><tr><td align="center" style="font-size:22px><b>Ventas de Seguros</b></td></tr></table>';
 			
 		
 	$from = 'operaciones@segurosexpress.com';
